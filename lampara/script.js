281232e5c107050a63b513f0f1117f50cdcf9e9b
javiermedinaj <81499144+javiermedinaj@users.noreply.github.com>
2021-11-15 14:34:04 -0300

    Add files via upload

---
+++ lampara/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn");
+const light = document.getElementById("light");
+
+
+function toggleBtn() {
+    btn.classList.toggle("active");
+    light.classList.toggle("on")
+}
